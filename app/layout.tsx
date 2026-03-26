d62690b5c3720c74c0b949380c8ae5bac186dced
Yuulis <yuulis2005@gmail.com>
2026-03-26 21:41:30 +0900

    feat: サイドバーのデータ管理を改善するために SidebarDataProvider を追加し、関連コンポーネントでのデータ取得方法を更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SidebarDataProvider } from "@/components/sidebar-data-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +36,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <SidebarDataProvider>
+            {children}
+          </SidebarDataProvider>
           <Toaster />
         </ThemeProvider>
       </body>
